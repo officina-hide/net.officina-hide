01fe6958ada6cd9df7be25f0e5568f8950dd0c22
hideo ueno <hideo.ueno@kacoms.co.jp>
2022-11-10 08:39:10 +0900

    Completion of building table information
    テーブル情報構築完了

--- src/net/officina_hide/base/model/I_FD_DB.java
+++ src/net/officina_hide/base/model/I_FD_DB.java
@@ -19,11 +19,42 @@ public interface I_FD_DB {
 	/** データベースユーザーパスワード */
 	public final String DB_USER_PASSWORD = "dbUserPassword";
 	
+	/** 共通項目 : 登録日 */
+	public final String COLUMNNAME_Created = "created";
+	public final String COMMENT_Created = "登録日";
+	/** 共通項目 : 登録者ID */
+	public final String COLUMNNAME_CreatedBy = "createdBy";
+	public final String COMMENT_CreatedBy = "登録者ID";
+	/** 共通項目 : 更新日 */
+	public final String COLUMNNAME_Updated = "updated";
+	public final String COMMENT_Updated = "更新日";
+	/** 共通項目 : 更新者ID */
+	public final String COLUMNNAME_UpdatedBy = "updatedBy";
+	public final String COMMENT_UpdatedBy = "更新者ID";
+	/** 汎用項目 : 名前 */
+	public final String COLUMNNAME_FD_Name = "FD_Name";
+	public final String COMMENT_FD_Name = "名前";
+	
 	/** SQL用カラム属性設定[Column attribute setting for SQL] */
 	public final String COLUMN_TYPE_ID_KEY = " bigint unsigned NOT NULL PRIMARY KEY ";
+	public final String COLUMN_TYPE_ID = " bigint unsigned ";
 	/** SQL用コメント[Comments for SQL] */
 	public final String COMMENT = " COMMENT ";
+	/** SQL用カラム属性 : 文字列 */
+	public final String COLUMN_TYPE_VARCHAR = " varchar(n) ";
+	/** SQL用カラム属性 : 日時 */
+	public final String COLUMN_TYPE_DATETIME = " datetime ";
 	/** シングルコーテーション[single quotes] */
-	public final static String FD_SQ = "'";
+	public final String FD_SQ = "'";
+	/** 共通項目追加用SQL[SQL for adding common items] */
+	public final String SQL_CREATE_COMMON_COLUMN =
+			COLUMNNAME_Created + COLUMN_TYPE_DATETIME
+				+ COMMENT + FD_SQ + COMMENT_Created + FD_SQ + " "
+			+ "," + COLUMNNAME_CreatedBy + COLUMN_TYPE_ID
+				+ COMMENT + FD_SQ + COMMENT_CreatedBy + FD_SQ + " "
+			+ "," + COLUMNNAME_Updated + COLUMN_TYPE_DATETIME
+				+ COMMENT + FD_SQ + COMMENT_Updated + FD_SQ + " "
+			+ "," + COLUMNNAME_UpdatedBy + COLUMN_TYPE_ID
+				+ COMMENT + FD_SQ + COMMENT_UpdatedBy + FD_SQ + " ";
 
 }
--- src/net/officina_hide/base/model/I_FD_Table.java
+++ src/net/officina_hide/base/model/I_FD_Table.java
@@ -15,7 +15,10 @@ public interface I_FD_Table extends I_FD_DB {
 	/** テーブル情報ID */
 	public final String COLUMNNAME_FD_Table_ID = Table_Name + "_ID";
 	public final String COMMENT_FD_Table_ID = Table_Comment + "ID";
-	
+	/** テーブル名 */
+	public final String COLUMNNAME_FD_Table_Name = Table_Name;
+	public final String COMMENT_FD_Table_Name = "テーブル名";
+	public final int SIZE_FD_Table_Name = 100;
 
 	/** テーブル情報構築用SQL[SQL for building table information] */
 	public final String SQL_DROP_FD_TABLE = 
@@ -24,6 +27,12 @@ public interface I_FD_Table extends I_FD_DB {
 			"CREATE TABLE IF NOT EXISTS " + Table_Name + " ("
 			+ COLUMNNAME_FD_Table_ID + COLUMN_TYPE_ID_KEY 
 				+ COMMENT + FD_SQ + COMMENT_FD_Table_ID + FD_SQ
+			+ "," + COLUMNNAME_FD_Table_Name 
+				+ COLUMN_TYPE_VARCHAR.replaceAll("n", Integer.toString(SIZE_FD_Table_Name))
+				+ COMMENT + FD_SQ + COMMENT_FD_Table_Name + FD_SQ + " "
+			+ "," + COLUMNNAME_FD_Name + COLUMN_TYPE_VARCHAR.replaceAll("n", "200")
+				+ COMMENT + FD_SQ + COMMENT_FD_Name + FD_SQ + " "
+			+ "," + SQL_CREATE_COMMON_COLUMN
 			+ ")"
 			+ COMMENT + FD_SQ + Table_Comment + FD_SQ
 			+";";
