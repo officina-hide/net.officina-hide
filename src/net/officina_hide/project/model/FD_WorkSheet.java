f582b7939cd28d8657410a4b5078f99899598dbf
hideo ueno <hideo.ueno@kacoms.co.jp>
2022-12-02 08:38:15 +0900

    WorkBenchのUpdate

---
+++ src/net/officina_hide/project/model/FD_WorkSheet.java
@@ -0,0 +1,11 @@
+package net.officina_hide.project.model;
+
+/**
+ * 作業票クラス[Work sheet class]
+ * @author officina-hide.net
+ * @version 1.00 新規作成[New create]
+ * @since 2022/11/29 Ver. 1.00
+ */
+public class FD_WorkSheet {
+
+}
--- src/net/officina_hide/project/tools/WorkBench.java
+++ src/net/officina_hide/project/tools/WorkBench.java
@@ -1,19 +1,18 @@
 package net.officina_hide.project.tools;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import net.officina_hide.project.model.FD_WorkSheet;
 
 /**
  * ガントチャート表示[Gantt chart display]<br>
@@ -34,10 +33,17 @@ public class WorkBench extends Application {
 	private double iniHeight;
 	/** タイトル : 幅 */
 	private double titleWidth;
-	
+	/** 作業 : 高さ/件 */
+	private double lineHeight;
+	/** テスト用データテーブル */
+	private List<FD_WorkSheet> wlist = new ArrayList<>();
 	
 	@Override
 	public void start(Stage stage) throws Exception {
+		//作業票リスト生成
+		wlist = getWorkSheetList();
+		System.out.println(wlist.size());
+		
 		SplitPane sp = new SplitPane();
 		
 		ScrollPane  scPaneR = new ScrollPane();
@@ -52,10 +58,6 @@ public class WorkBench extends Application {
 		sp.getItems().addAll(scPaneR, scPaneL);
 		sp.setDividerPositions(0.2, 0.8);
 		
-//		ScrollPane  scPane = new ScrollPane();
-//		Pane pane = new Pane();
-//		pane.setPadding(new Insets(10, 10, 10, 10));
-//		
 		//システム画面のサイズ取得
 		dispWidth = Screen.getPrimary().getBounds().getWidth();
 		dispHeight = Screen.getPrimary().getBounds().getHeight();
@@ -65,37 +67,40 @@ public class WorkBench extends Application {
 		iniHeight = Math.abs(dispHeight * 0.8);
 		//タイトルの幅（表示画面サイズの20%を初期値とする。)
 		titleWidth = Math.abs(iniWidth * 0.2);
+		//1行高さ
+		lineHeight = 28;
 		
 		//タイトル枠
-		VBox rbox = new VBox(5);
-		rbox.setPrefWidth(titleWidth);
-		Label label = new Label("test");
-		
-		Rectangle titleBox = new Rectangle(titleWidth, iniHeight - 20);
+		Rectangle titleBox = new Rectangle(titleWidth, lineHeight * wlist.size());
 		titleBox.setX(10);
 		titleBox.setY(10);
 		titleBox.setStrokeWidth(2);
 		titleBox.setStroke(Color.DARKGRAY);
 		titleBox.setFill(Color.WHITE);
-
-//		rbox.getChildren().addAll(label, titleBox);
-		
 		paneR.getChildren().addAll(titleBox);
-		
-//		paneR.getChildren().addAll(rbox);
-		
-//		
-//		Rectangle mainBox = new Rectangle(iniWidth, iniHeight);
-//		mainBox.setX(10);
-//		mainBox.setY(10);
-//		mainBox.setStrokeWidth(2);
-//		mainBox.setStroke(Color.DARKGRAY);
-//		mainBox.setFill(Color.WHITE);
-//		pane.getChildren().add(mainBox);
+		//バーチャート枠
+		Rectangle barBox = new Rectangle(iniWidth * 0.8, lineHeight * wlist.size() + 1);
+		barBox.setStrokeWidth(2);
+		barBox.setStroke(Color.DARKGRAY);
+		barBox.setFill(Color.WHITE);
+		paneL.getChildren().addAll(barBox);
 		
 		Scene scene = new Scene(sp, iniWidth, iniHeight);
 		stage.setScene(scene);
 		stage.show();
+	}
+
+	/**
+	 * 作業票リスト取得[Getting Work Sheet List]<br>
+	 * @author officina-hide.net
+	 * @since 2022/11/29 Ver. 1.00
+	 * @return 作業票リスト[Work sheet list]
+	 */
+	private List<FD_WorkSheet> getWorkSheetList() {
+		List<FD_WorkSheet> list = new ArrayList<>();
+		list.add(new FD_WorkSheet());
+		list.add(new FD_WorkSheet());
+		return list;
 	}
 
 	public static void main(String[] args) {
